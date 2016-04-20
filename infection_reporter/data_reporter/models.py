a1931087047bae57b36fc10178576397e9bb3274
Vango Stavro <vangjelstavro@hotmail.com>
2016-04-20 23:51:18 +0200

    Refactoring

--- infection_reporter/data_reporter/models.py
+++ infection_reporter/data_reporter/models.py
@@ -17,6 +17,9 @@ class Disease(StdModel):
     name = models.CharField('Name', max_length=255)
     incubation_period = models.PositiveIntegerField('Incubation period')
     symptoms = models.CharField('Symptoms', max_length=255)
+    sequela = models.BooleanField('Sequela', default=False)
+    human = models.BooleanField('Human', default=False)
+    animal = models.BooleanField('Animal', default=False)
 
     def __str__(self):
         return self.name
@@ -29,17 +32,21 @@ class HumanCase(StdModel):
         verbose_name_plural = 'Human Cases'
 
     case_count = models.PositiveIntegerField('Number of cases')
-    residency_region = models.CharField('Region', max_length=255)
-    residency_district = models.CharField('District', max_length=255)
-    residency_commune = models.CharField('Commune', max_length=255)
-    residency_village = models.CharField('Village', max_length=255)
-    start_period = models.DateTimeField('Start Date')
-    end_period = models.DateTimeField('End Date')
+    hospitalization_avg = models.PositiveIntegerField('Hospitalization average time ')
+    sampling_avg = models.PositiveIntegerField('Sampling average time')
+    laboratory_avg = models.PositiveIntegerField('Laboratory average time')
+    report_avg = models.PositiveIntegerField('Report average time')
+    district = models.CharField('District', max_length=255)
+    village = models.CharField('Village', max_length=255)
+    min_age = models.DecimalField('Min. Age', decimal_places=1, max_digits=3)
+    max_age = models.DecimalField('Max Age', decimal_places=1, max_digits=3)
+    avg_age = models.DecimalField('Average Age', decimal_places=1, max_digits=3)
+    vaccination_rate = models.DecimalField('Vaccination Rate', decimal_places=1, max_digits=3)
+    death_rate = models.DecimalField('Death Rate', decimal_places=1, max_digits=3)
+    recovery_rate = models.DecimalField('Recovery Rate', decimal_places=1, max_digits=3)
+    sequela_rate = models.DecimalField('Sequela Rate', decimal_places=1, max_digits=3)
+    attack_rate = models.DecimalField('Attack Rate', decimal_places=1, max_digits=3)
     disease = models.ForeignKey(Disease)
-    confirm_count = models.PositiveIntegerField('Confirmed')
-    probable_count = models.PositiveIntegerField('Probable')
-    suspected_count = models.PositiveIntegerField('Suspected')
-    severity_level = models.CharField('Severity Level', max_length=255)
 
     def __str__(self):
         return "{} - {} - {}".format(self.disease, self.case_count, self.residency_region)
@@ -52,14 +59,9 @@ class AnimalCase(StdModel):
         verbose_name_plural = 'Animal Cases'
 
     case_count = models.PositiveIntegerField('Number of cases')
-    residency_region = models.CharField('Region', max_length=255)
-    residency_district = models.CharField('District', max_length=255)
-    residency_commune = models.CharField('Commune', max_length=255)
-    residency_village = models.CharField('Village', max_length=255)
-    start_period = models.DateTimeField('Start Date')
-    end_period = models.DateTimeField('End Date')
+    district = models.CharField('District', max_length=255)
+    village = models.CharField('District', max_length=255)
+    morbidity_rate = models.DecimalField('Morbidity rate', max_digits=3, decimal_places=2)
+    mortality_rate = models.DecimalField('Mortality rate', max_digits=3, decimal_places=2)
+    fatality_rate = models.DecimalField('Fatality rate', max_digits=3, decimal_places=2)
     disease = models.ForeignKey(Disease)
-    confirm_count = models.PositiveIntegerField('Confirmed')
-    probable_count = models.PositiveIntegerField('Probable')
-    suspected_count = models.PositiveIntegerField('Suspected')
-    severity_level = models.CharField('Severity Level', max_length=255)
